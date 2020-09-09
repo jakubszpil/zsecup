962d14df5d7f05a523ca6fc471a9bff740928796
Jakub Szpil <kuba0122@gmail.com>
2020-09-09 18:15:33 +0200

    Update - version development 0.09

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,7 @@ export default {
   modules: ['@nuxtjs/axios', '@nuxt/content', '@nuxtjs/proxy', '@nuxtjs/style-resources'],
   proxy: {
     '/api': {
-      target: 'http://localhost/services',
+      target: 'http://esportwzse.pl',
       pathRewrite: {
         '^/api': '/',
       },
